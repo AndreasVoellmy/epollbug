d9bcb5ad15f326e1bf88cc9df4da06ab0c4a3082
Kazu Yamamoto <kazu@iij.ad.jp>
2013-02-07 18:18:03 +0900

    deleting unnecessary white spaces.

--- kqueueserver.c
+++ kqueueserver.c
@@ -44,22 +44,22 @@ void *socketCheck(void *);
 #define NUM_CLIENTS 500
 
 // Fill this in with the http request that your
-// weighttp client sends to the server. This is the 
+// weighttp client sends to the server. This is the
 // request that I get. You can also find out what weighttp
 // is sending by first setting SHOW_REQUEST (see below) and leaving
 // this as is. Then when you run the program it will print the request
 // it received.
-char EXPECTED_HTTP_REQUEST[] = 
+char EXPECTED_HTTP_REQUEST[] =
   "GET / HTTP/1.1\r\nHost: 10.12.0.1:8080\r\n"
   "User-Agent: weighttp/0.3\r\nConnection: keep-alive\r\n\r\n";
 
-// Define this and the program will print the request made 
+// Define this and the program will print the request made
 // by the http client and then exit.
-// #define SHOW_REQUEST 
+// #define SHOW_REQUEST
 
 int EXPECTED_RECV_LEN;
 
-char RESPONSE[] = 
+char RESPONSE[] =
   "HTTP/1.1 200 OK\r\n"
   "Date: Tue, 09 Oct 2012 16:36:18 GMT\r\n"
   "Content-Length: 147\r\n"
@@ -113,7 +113,7 @@ void startWorkerThread(int w) {
 void *workerLoop(void * arg) {
   int w = (int)(unsigned long) arg;
   int epfd = workers[w].efd;
-  int n; 
+  int n;
   int i;
   int sock;
   struct kevent64_s *events;
@@ -156,7 +156,7 @@ void receiveLoop(int sock, int epfd, char recvbuf[]) {
 	if (numSent != RESPONSE_LEN) {
 	  perror("partial send");
 	  exit(-1);
-	} 
+	}
       } else {
 	perror("partial recv");
 	exit(-1);
@@ -167,9 +167,9 @@ void receiveLoop(int sock, int epfd, char recvbuf[]) {
 	// re-arm the socket with epoll.
 	event.ident = sock;
 	event.filter = EVFILT_READ;
-	event.flags = EV_ADD | EV_ONESHOT; 
+	event.flags = EV_ADD | EV_ONESHOT;
 	if (kevent64(epfd, &event, 1, NULL, 0, 0, NULL)) {
-	  perror("rearm"); 
+	  perror("rearm");
 	  exit(-1);
 	}
 	break;
@@ -188,11 +188,11 @@ void startSocketCheckThread(void) {
     perror("pthread_create");
     exit(-1);
   }
-  return; 
+  return;
 }
 
 void *socketCheck(void * arg) {
-  int i, bytesAvailable; 
+  int i, bytesAvailable;
   sleep(10);
   for (i = 0; i < NUM_CLIENTS; i++) {
     if (ioctl(sockets[i], FIONREAD, &bytesAvailable) < 0) {
@@ -200,7 +200,7 @@ void *socketCheck(void * arg) {
       exit(-1);
     }
     if (bytesAvailable > 0) {
-      printf("socket %d has %d bytes of data ready\n", sockets[i], bytesAvailable);  
+      printf("socket %d has %d bytes of data ready\n", sockets[i], bytesAvailable);
     }
   }
   pthread_exit(NULL);
@@ -214,29 +214,29 @@ void acceptLoop(void)
   socklen_t alen = sizeof(addr);
   short port = PORT_NUM;
   int sock_tmp;
-  int current_worker = 0; 
+  int current_worker = 0;
   int current_client = 0;
   int optval;
 
   if (-1 == (sd = socket(PF_INET, SOCK_STREAM, 0))) {
     printf("socket: error: %d\n",errno);
     exit(-1);
-  }   
-  
+  }
+
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = INADDR_ANY;
   addr.sin_port = htons(port);
-  
+
   optval = 1;
   setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
   if (bind(sd, (struct sockaddr*)&addr, sizeof(addr))) {
     printf("bind error: %d\n",errno);
     exit(-1);
-  }   
+  }
   if (listen(sd, BACKLOG)) {
     printf("listen error: %d\n",errno);
     exit(-1);
-  }   
+  }
   while(1) {
     if (-1 == (sock_tmp = accept(sd, (struct sockaddr*)&addr, &alen))) {
       printf("Error %d doing accept", errno);
@@ -249,7 +249,7 @@ void acceptLoop(void)
     event.filter = EVFILT_READ ;
     event.flags = EV_ADD | EV_ONESHOT;
     kevent64(workers[current_worker].efd, &event, 1, NULL, 0, 0, NULL);
-    current_client++;    
+    current_client++;
     current_worker = (current_worker + 1) % NUM_WORKERS;
   }
 }
--- kqueueserver2.c
+++ kqueueserver2.c
@@ -38,22 +38,22 @@ void socketCheck(void);
 #define NUM_CLIENTS 10 // 500 // comes from the -c argument of weighttp
 
 // Fill this in with the http request that your
-// weighttp client sends to the server. This is the 
+// weighttp client sends to the server. This is the
 // request that I get. You can also find out what weighttp
 // is sending by first setting SHOW_REQUEST (see below) and leaving
 // this as is. Then when you run the program it will print the request
 // it received.
-char EXPECTED_HTTP_REQUEST[] = 
+char EXPECTED_HTTP_REQUEST[] =
   "GET / HTTP/1.1\r\nHost: 10.12.0.1:8080\r\n"
   "User-Agent: weighttp/0.3\r\nConnection: keep-alive\r\n\r\n";
 
-// Define this and the program will print the request made 
+// Define this and the program will print the request made
 // by the http client and then exit.
-// #define SHOW_REQUEST 
+// #define SHOW_REQUEST
 
 int EXPECTED_RECV_LEN;
 
-char RESPONSE[] = 
+char RESPONSE[] =
   "HTTP/1.1 200 OK\r\n"
   "Date: Tue, 09 Oct 2012 16:36:18 GMT\r\n"
   "Content-Length: 145\r\n"
@@ -98,7 +98,7 @@ void startWorkerThread(int w) {
 void *workerLoop(void * arg) {
   int w = (int)(unsigned long) arg;
   int epfd;
-  int n; 
+  int n;
   int i;
   int sock;
   struct kevent64_s *events;
@@ -118,7 +118,7 @@ void *workerLoop(void * arg) {
       event.ident =  sockets[j];
       event.filter = EVFILT_READ ;
       event.flags = EV_ADD | EV_ONESHOT;
-      kevent64(epfd, &event, 1, NULL, 0, 0, NULL); 
+      kevent64(epfd, &event, 1, NULL, 0, 0, NULL);
     }
   }
 
@@ -168,7 +168,7 @@ void receiveLoop(int sock, int epfd, int w, char recvbuf[]) {
 	if (numSent != RESPONSE_LEN) {
 	  perror("partial send");
 	  exit(-1);
-	} 
+	}
       } else {
 	perror("partial recv");
 	exit(-1);
@@ -179,9 +179,9 @@ void receiveLoop(int sock, int epfd, int w, char recvbuf[]) {
 	  // re-arm the socket with epoll.
 	  event.ident = sock;
 	  event.filter = EVFILT_READ;
-	  event.flags = EV_ADD | EV_ONESHOT; 
+	  event.flags = EV_ADD | EV_ONESHOT;
 	  if (kevent64(epfd, &event, 1, NULL, 0, 0, NULL)) {
-	    perror("rearm"); 
+	    perror("rearm");
 	    exit(-1);
 	  }
 	  printf("rearmed %d on %d\n", sock, w);
@@ -199,7 +199,7 @@ void receiveLoop(int sock, int epfd, int w, char recvbuf[]) {
 }
 
 void socketCheck() {
-  int i, bytesAvailable; 
+  int i, bytesAvailable;
   sleep(10);
   for (i = 0; i < NUM_CLIENTS; i++) {
     if (ioctl(sockets[i], FIONREAD, &bytesAvailable) < 0) {
@@ -207,12 +207,12 @@ void socketCheck() {
       exit(-1);
     }
     if (bytesAvailable > 0) {
-      printf("socket %d with index %d assigned to worker %d has %d bytes of data ready and completed %d requests\n", 
-	     sockets[i], 
+      printf("socket %d with index %d assigned to worker %d has %d bytes of data ready and completed %d requests\n",
+	     sockets[i],
 	     i,
-	     socketAssignments[i], 
-	     bytesAvailable, 
-	     socketRequestCounts[i]);  
+	     socketAssignments[i],
+	     bytesAvailable,
+	     socketRequestCounts[i]);
     }
   }
 }
@@ -224,29 +224,29 @@ void acceptLoop(void)
   socklen_t alen = sizeof(addr);
   short port = PORT_NUM;
   int sock_tmp;
-  int current_worker = 0; 
+  int current_worker = 0;
   int current_client = 0;
   int optval;
   int j;
   if (-1 == (sd = socket(PF_INET, SOCK_STREAM, 0))) {
     printf("socket: error: %d\n",errno);
     exit(-1);
-  }   
-  
+  }
+
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = INADDR_ANY;
   addr.sin_port = htons(port);
-  
+
   optval = 1;
   setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
   if (bind(sd, (struct sockaddr*)&addr, sizeof(addr))) {
     printf("bind error: %d\n",errno);
     exit(-1);
-  }   
+  }
   if (listen(sd, BACKLOG)) {
     printf("listen error: %d\n",errno);
     exit(-1);
-  }   
+  }
   for (j=0; j<NUM_CLIENTS; j++) {
     if (-1 == (sock_tmp = accept(sd, (struct sockaddr*)&addr, &alen))) {
       printf("Error %d doing accept", errno);
